279b3a3e4babb39e9336299f137350b878ccb012
David Bowen <10568923+DavidSBowen@users.noreply.github.com>
2026-07-24 17:12:30 -0400

    Add: reverse-a-linked-list - submission-45

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-45.ts
@@ -0,0 +1,63 @@
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        // 1 -> 2 -> 3 -> null
+        // 1 current
+        // null previous
+        // loop while current
+            // 2 actual next 
+            // current.next = previous
+            // 1 -> null
+            // previous = current
+            // 1 (1 -> null)
+            // current = next
+            // 2 (2 -> null)
+            // current = 2 (2 -> null); previous = 1 (1 -> null)
+                // 3 actual next
+                // current.next = previous
+                // 1 -> 2 (2 -> 1 -> null)
+                // previous = current
+                // 2 (2 -> 1 -> null)
+                // current = next
+                // 3
+                // current = 3 (3 -> null); previous = 2 (2 -> 1 -> null)
+                    // null actual next
+                    // current.next = previous
+                    // 3 -> 2 (2 -> 1 -> null)
+                    // previous = current
+                    // 3 (3 -> 2 -> 1 -> null)
+                    // current = actual next
+                    // null
+        // return previous (3)
+
+        // let current = head
+        // let previous = null
+
+        // while (current) {
+        //     let next = current.next
+        //     current.next = previous
+        //     previous = current
+        //     current = next
+        // }
+
+        // return previous
+
+        // 1 -> 2 -> 3 -> null
+        //           3 -> 2 (2 -> 3)
+        //                  (2 -> null)
+        //           3 -> 2 -> null
+        //      2 -> 
+
+        if (!head) return null
+        let newHead = head
+        while (head.next) {
+            newHead = this.reverseList(head.next)
+            head.next.next = head
+            head.next = null
+        }
+        return newHead
+    }
+}
