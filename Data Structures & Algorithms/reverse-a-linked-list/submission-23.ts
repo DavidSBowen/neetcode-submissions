4a4167f800e2d0cf9358f58f2818c782f0e000dc
David Bowen <10568923+DavidSBowen@users.noreply.github.com>
2026-07-24 12:27:44 -0400

    Add: reverse-a-linked-list - submission-23

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-23.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        
+        let current: ListNode | null = head
+        let previous: ListNode | null = null
+
+        while (current) {
+            let next = current.next
+            current.next = previous
+            previous = current
+            current = next
+
+        }
+
+        return previous
+    }
+}
